3eca5790355a0210ba51e6b34d56e1645fde6f1e
trevorswan11 <tcs94@case.edu>
2026-06-20 00:28:55 -0400

    refactor: Use better names in arena impl

--- include/stdx/arena.hh
+++ include/stdx/arena.hh
@@ -28,10 +28,10 @@ template <usize BlockSize = DEFAULT_ARENA_BLOCK_SIZE> class arena {
     arena(const arena&)                    = delete;
     auto operator=(const arena&) -> arena& = delete;
     arena(arena&& other) noexcept
-        : offset_{other.offset_}, block_head_{other.block_head_},
+        : block_offset_{other.block_offset_}, block_head_{other.block_head_},
           block_current_{other.block_current_}, cleanup_current_{other.cleanup_current_},
           cleanup_head_{other.cleanup_head_} {
-        other.offset_          = 0;
+        other.block_offset_    = 0;
         other.block_head_      = nullptr;
         other.block_current_   = nullptr;
         other.cleanup_head_    = nullptr;
@@ -46,11 +46,11 @@ template <usize BlockSize = DEFAULT_ARENA_BLOCK_SIZE> class arena {
         requires(sizeof(T) + alignof(T) <= BlockSize)
     [[nodiscard]] auto make(Args&&... args) -> gsl::not_null<T*> {
         void* mem{alloc(sizeof(T), alignof(T))};
-        auto* obj{new (mem) T{std::forward<Args>(args)...}};
+        auto* data{new (mem) T{std::forward<Args>(args)...}};
         if constexpr (!TriviallyDestructible<T>) {
-            register_destructors(obj, [](void* ptr) -> void { static_cast<T*>(ptr)->~T(); });
+            register_destructor(data, [](void* ptr) -> void { static_cast<T*>(ptr)->~T(); });
         }
-        return obj;
+        return data;
     }
 
     // Asserts that the requested type-count product can fit in the block size
@@ -62,26 +62,27 @@ template <usize BlockSize = DEFAULT_ARENA_BLOCK_SIZE> class arena {
         const auto size{sizeof(T) * count};
         ASSERT(size + alignof(T) - 1 <= BlockSize, "Block size cannot fit requested type count");
         void* mem{alloc(size, alignof(T))};
-        auto* ptr{static_cast<T*>(mem)};
-        for (usize i{0}; i < count; ++i) { new (static_cast<void*>(ptr + i)) T{}; }
+        auto* data{static_cast<T*>(mem)};
+        for (usize i{0}; i < count; ++i) { new (static_cast<void*>(data + i)) T{}; }
 
         if constexpr (!TriviallyDestructible<T>) {
-            void* ctx_mem{alloc(sizeof(span_context<T>), alignof(span_context<T>))};
-            auto* ctx{new (ctx_mem) span_context<T>{ptr, count}};
+            using cleanup_span = cleanup_node::template span<T>;
+            void* ctx_mem{alloc(sizeof(cleanup_span), alignof(cleanup_span))};
+            auto* ctx{new (ctx_mem) cleanup_span{data, count}};
 
-            register_destructors(ctx, [](void* ptr) -> void {
-                auto* context{static_cast<span_context<T>*>(ptr)};
+            register_destructor(ctx, [](void* ptr) -> void {
+                auto* context{static_cast<cleanup_span*>(ptr)};
                 for (usize i{0}; i < context->size; ++i) { (context->data + i)->~T(); }
             });
         }
-        return gsl::span{ptr, count};
+        return gsl::span{data, count};
     }
     // cppcheck-suppress-end [unreadVariable, internalAstError]
 
     auto reset() noexcept -> void {
         run_destructors();
         block_current_ = block_head_;
-        offset_        = 0;
+        block_offset_  = 0;
     }
 
     // Deallocates all memory associated with the arena.
@@ -100,67 +101,17 @@ template <usize BlockSize = DEFAULT_ARENA_BLOCK_SIZE> class arena {
     }
 
   private:
-    auto register_destructors(void* ptr, void (*destroy_fn)(void*)) -> void {
-        void* node_mem{alloc(sizeof(cleanup_node), alignof(cleanup_node))};
-        auto* node{new (node_mem) cleanup_node{
-            .destroy     = destroy_fn,
-            .storage_ptr = ptr,
-            .next        = nullptr,
-        }};
+    using destructor_t = void (*)(void*);
 
-        if (cleanup_current_) {
-            cleanup_current_->next = node;
-            cleanup_current_       = node;
-        } else {
-            cleanup_head_    = node;
-            cleanup_current_ = cleanup_head_;
-        }
-    }
-
-    auto run_destructors() noexcept -> void {
-        cleanup_node* current_cleanup{cleanup_head_};
-        while (current_cleanup) {
-            current_cleanup->destroy(current_cleanup->storage_ptr);
-            current_cleanup = current_cleanup->next;
-        }
-        cleanup_head_    = nullptr;
-        cleanup_current_ = nullptr;
-    }
-
-    [[nodiscard]] auto alloc(usize size, usize align) -> void* {
-        PROFILE_FUNCTION();
-        if (block_current_) {
-            auto        raw_addr{reinterpret_cast<uptr>(block_current_ + 1)};
-            uptr        current_ptr{raw_addr + offset_};
-            uptr        aligned_ptr{(current_ptr + (align - 1)) & ~(align - 1)};
-            const usize total_size{aligned_ptr - raw_addr + size};
-
-            if (total_size <= BlockSize) {
-                offset_ = total_size;
-                return reinterpret_cast<void*>(aligned_ptr);
-            }
-
-            if (block_current_->next) {
-                block_current_ = block_current_->next;
-                offset_        = 0;
-                return alloc(size, align);
-            }
-        }
-
-        // Otherwise a new block needs to be created for the memory
-        return block::alloc(*this, size, align);
-    }
-
-  private:
     struct cleanup_node {
-        void (*destroy)(void*);
-        void*         storage_ptr;
-        cleanup_node* next;
-    };
+        template <typename T> struct span {
+            T*    data;
+            usize size;
+        };
 
-    template <typename T> struct span_context {
-        T*    data;
-        usize size;
+        destructor_t  destructor;
+        void*         data;
+        cleanup_node* next;
     };
 
     struct block {
@@ -172,21 +123,71 @@ template <usize BlockSize = DEFAULT_ARENA_BLOCK_SIZE> class arena {
             void* raw{::operator new(sizeof(block) + BlockSize)};
             auto* blk{new (raw) block{}};
 
-            if (!a.block_head_) {
-                a.block_head_ = blk;
-            } else {
-                ASSERT(a.block_current_);
+            if (a.block_current_) {
                 a.block_current_->next = blk;
+            } else {
+                a.block_head_ = blk;
             }
-
             a.block_current_ = blk;
-            a.offset_        = 0;
+
+            a.block_offset_ = 0;
             return a.alloc(size, align);
         }
     };
 
   private:
-    usize         offset_{0};
+    auto register_destructor(void* data, destructor_t destructor) -> void {
+        void* node_mem{alloc(sizeof(cleanup_node), alignof(cleanup_node))};
+        auto* node{new (node_mem) cleanup_node{
+            .destructor = destructor,
+            .data       = data,
+            .next       = nullptr,
+        }};
+
+        if (cleanup_current_) {
+            cleanup_current_->next = node;
+        } else {
+            cleanup_head_ = node;
+        }
+        cleanup_current_ = node;
+    }
+
+    auto run_destructors() noexcept -> void {
+        cleanup_node* current_cleanup{cleanup_head_};
+        while (current_cleanup) {
+            current_cleanup->destructor(current_cleanup->data);
+            current_cleanup = current_cleanup->next;
+        }
+        cleanup_head_    = nullptr;
+        cleanup_current_ = nullptr;
+    }
+
+    [[nodiscard]] auto alloc(usize size, usize align) -> void* {
+        PROFILE_FUNCTION();
+        if (block_current_) {
+            auto        raw_addr{reinterpret_cast<uptr>(block_current_ + 1)};
+            uptr        current_ptr{raw_addr + block_offset_};
+            uptr        aligned_ptr{(current_ptr + (align - 1)) & ~(align - 1)};
+            const usize total_size{aligned_ptr - raw_addr + size};
+
+            if (total_size <= BlockSize) {
+                block_offset_ = total_size;
+                return reinterpret_cast<void*>(aligned_ptr);
+            }
+
+            if (block_current_->next) {
+                block_current_ = block_current_->next;
+                block_offset_  = 0;
+                return alloc(size, align);
+            }
+        }
+
+        // Otherwise a new block needs to be created for the memory
+        return block::alloc(*this, size, align);
+    }
+
+  private:
+    usize         block_offset_{0};
     block*        block_head_{nullptr};
     block*        block_current_{nullptr};
     cleanup_node* cleanup_head_{nullptr};
