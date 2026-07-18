dcccb620228e9c63fc9507e2f92c3ecd2153b294
trevorswan11 <tcs94@case.edu>
2026-07-18 21:28:43 +0000

    Deploying to coverage from @ trevorswan11/stdx@37ce50e63467dc91d633bb94882c35f8d736b63d 🚀

---
+++ merged/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"94.7","covered_lines":"853","uncovered_lines":"48","total_lines" : "901"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2026-07-18 21:28:42", "instrumented" : 901, "covered" : 853,};
---
+++ merged/kcov-merged/index.js
@@ -0,0 +1,23 @@
+var data = {files:[
+{"link":"string.hh.a2e0ebbc.html","title":"string.hh","summary_name":"[...]/stdx/stdx/include/stdx/string.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"22","uncovered_lines":"0","total_lines" : "22"},
+{"link":"storage.hh.6c5f1de0.html","title":"storage.hh","summary_name":"[...]/stdx/stdx/include/stdx/fixed/storage.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"6","uncovered_lines":"0","total_lines" : "6"},
+{"link":"math.hh.cc356842.html","title":"math.hh","summary_name":"[...]/stdx/stdx/include/stdx/math.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"12","uncovered_lines":"0","total_lines" : "12"},
+{"link":"profiler.cc.b74afb84.html","title":"profiler.cc","summary_name":"[...]/stdx/stdx/src/profiler.cc","covered_class":"lineCov","covered":"100.0","covered_lines":"4","uncovered_lines":"0","total_lines" : "4"},
+{"link":"hash.hh.2c2d9bf5.html","title":"hash.hh","summary_name":"[...]/stdx/stdx/include/stdx/hash.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"32","uncovered_lines":"0","total_lines" : "32"},
+{"link":"vector.hh.bf8613e7.html","title":"vector.hh","summary_name":"[...]/stdx/stdx/include/stdx/fixed/vector.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"76","uncovered_lines":"0","total_lines" : "76"},
+{"link":"utility.hh.356170b5.html","title":"utility.hh","summary_name":"[...]/stdx/stdx/include/stdx/utility.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"1","uncovered_lines":"0","total_lines" : "1"},
+{"link":"result.hh.ef85ac0f.html","title":"result.hh","summary_name":"[...]/stdx/stdx/include/stdx/result.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"2","uncovered_lines":"0","total_lines" : "2"},
+{"link":"option.hh.4fe60723.html","title":"option.hh","summary_name":"[...]/stdx/stdx/include/stdx/option.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"73","uncovered_lines":"0","total_lines" : "73"},
+{"link":"variant.hh.7f811bc1.html","title":"variant.hh","summary_name":"[...]/stdx/stdx/include/stdx/variant.hh","covered_class":"lineCov","covered":"98.8","covered_lines":"83","uncovered_lines":"1","total_lines" : "84"},
+{"link":"enum.hh.4324d72.html","title":"enum.hh","summary_name":"[...]/stdx/stdx/include/stdx/enum.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"15","uncovered_lines":"0","total_lines" : "15"},
+{"link":"json.hh.75d70c13.html","title":"json.hh","summary_name":"[...]/stdx/stdx/include/stdx/json.hh","covered_class":"lineCov","covered":"83.3","covered_lines":"15","uncovered_lines":"3","total_lines" : "18"},
+{"link":"arena.hh.f8461da8.html","title":"arena.hh","summary_name":"[...]/stdx/stdx/include/stdx/arena.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"105","uncovered_lines":"0","total_lines" : "105"},
+{"link":"memory.hh.24ea16ff.html","title":"memory.hh","summary_name":"[...]/stdx/stdx/include/stdx/memory.hh","covered_class":"lineCov","covered":"95.2","covered_lines":"40","uncovered_lines":"2","total_lines" : "42"},
+{"link":"hash_table.hh.426833a1.html","title":"hash_table.hh","summary_name":"[...]/stdx/stdx/include/stdx/fixed/hash_table.hh","covered_class":"lineCov","covered":"90.7","covered_lines":"262","uncovered_lines":"27","total_lines" : "289"},
+{"link":"string.hh.61466f3f.html","title":"string.hh","summary_name":"[...]/stdx/stdx/include/stdx/fixed/string.hh","covered_class":"lineCov","covered":"95.7","covered_lines":"89","uncovered_lines":"4","total_lines" : "93"},
+{"link":"assert.hh.1c1ac16f.html","title":"assert.hh","summary_name":"[...]/stdx/stdx/include/stdx/assert.hh","covered_class":"lineNoCov","covered":"21.4","covered_lines":"3","uncovered_lines":"11","total_lines" : "14"},
+{"link":"enum_map.hh.28359d09.html","title":"enum_map.hh","summary_name":"[...]/stdx/stdx/include/stdx/fixed/enum_map.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"13","uncovered_lines":"0","total_lines" : "13"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2026-07-18 21:28:42", "instrumented" : 901, "covered" : 853,};
+var merged_data = [];
